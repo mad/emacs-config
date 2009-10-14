2d07aa35958814d913b080d6ba50f434db1f9b35
mad <owner.mad.epa@gmail.com>
2009-10-14 13:32:07 +0400

    uncomment python support

--- init/init-pymacs.el
+++ init/init-pymacs.el
@@ -1,8 +1,9 @@
-;;; Python support
-;; (add-to-list 'load-path "~/git/pymacs")
-;; (require 'pymacs)
-;; (setenv "PYMACS_PYTHON" "python2.6")
-;; (pymacs-load "ropemacs" "rope-")
-;; (setq ropemacs-enable-autoimport t)
-;; (add-hook 'python-mode-hook 'ropemacs-mode)
+(add-to-list 'load-path "~/git/pymacs")
+(require 'pymacs)
+(setq pymacs-load-path '("/usr/lib/python2.6/site-packages/ropemacs/"
+                         "/usr/lib/python2.6/site-packages/ropemode/"))
+(setenv "PYMACS_PYTHON" "python2.6")
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+(add-hook 'python-mode-hook 'ropemacs-mode)
 
