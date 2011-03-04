8fe37d589c3af0ff9af24bb714fd4b313d9d4c95
Pavel Ershov <owner.mad.epa@gmail.com>
2011-03-04 22:26:07 +0300

    emacs 24 support prolog-mode out of the box, check it

--- init/init-prolog.el
+++ init/init-prolog.el
@@ -1,3 +1,6 @@
-(require 'prolog)
-(setq prolog-system 'swi)
+;; Now in 24.0 emacs prolog mode out of the box
+
+(when (version< emacs-version "24")
+  (require 'prolog)
+  (setq prolog-system 'swi))
 
